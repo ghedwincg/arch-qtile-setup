661261a27c350caa92245fd62c789d349ce4a06a
ghedwincg <122061582+ghedwincg@users.noreply.github.com>
2026-05-24 13:00:30 -0400

    Delete qtilesys directory

--- qtilesys/bin/dunst_history.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Simple script to show Dunst notification history in rofi
-
-# Show history
-dunstctl history | rofi -dmenu -p "Notifications"
-
-# Clear history if needed
-# dunstctl history-clear
--- qtilesys/bin/rofi-notification-sidebar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Rofi Notification Sidebar
-
-dunstctl history | rofi -dmenu \
-    -p "Notifications" \
-    -theme ~/.config/rofi/notification_sidebar.rasi
